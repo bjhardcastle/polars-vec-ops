ec1529fdd6ebe2ef0e0aeaab64688b68fd543b05
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-03-27 13:45:17 -0700

    Fix type hints

--- polars_vec_ops/__init__.py
+++ polars_vec_ops/__init__.py
@@ -461,7 +461,8 @@ class VecOpsNamespace:
 
         if not include_breakpoints and target is not None:
             # Unwrap to just counts list + cast dtype
-            def _unwrap_and_cast(s: pl.Series, _target: type[pl.DataType] = target) -> pl.Series:
+            _target_type = target
+            def _unwrap_and_cast(s: pl.Series, _target: type[pl.DataType] = _target_type) -> pl.Series:
                 return (
                     s.struct.field("counts")
                     .list.eval(pl.element().cast(_target))
@@ -479,7 +480,8 @@ class VecOpsNamespace:
             )
         elif target is not None:
             # Keep struct, cast counts dtype
-            def _cast_counts(s: pl.Series, _target: type[pl.DataType] = target) -> pl.Series:
+            _target_type = target
+            def _cast_counts(s: pl.Series, _target: type[pl.DataType] = _target_type) -> pl.Series:
                 fields = s.struct.unnest()
                 fields = fields.with_columns(
                     pl.col("counts").list.eval(
